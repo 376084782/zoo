c546691d36492871169a9e51d534c0874e357d5c
qianliyun <376084782@qq.com>
2021-12-31 03:24:19 +0800

    no message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,66 @@
 import API from "../api/API";
 import AgoraTokenGenerater from "../api/AgoraTokenGenerater";
 import Util from "../socket/Util";
+import ModelAnimalType from "../models/ModelAnimalType";
+import ModelUser from "../models/ModelUser";
+import ModelConfig from "../models/ModelConfig";
+import ModelConfigRoom from "../models/ModelConfigRoom";
 
 var express = require("express");
 var router = express.Router();
 /* GET home page. */
-router.post("/proxy", async (req, res, next) => {
+router.post("/room/save", async (req, res, next) => {
   let data = req.body;
-  let data2: any = await API.doAjax({
-    url: data.url,
-    method: data.method,
-    data: data.data
+  for (let i = 0; i < data.length; i++) {
+    let confEach = data[i];
+    if (typeof confEach.AP != 'string') {
+      confEach.AP = JSON.stringify(confEach.AP);
+    }
+    await ModelConfigRoom.updateOne({ id: confEach.id }, {
+      RP: confEach.RP,
+      PG: confEach.PG,
+      ZG: confEach.ZG,
+      AP: confEach.AP
+    });
+  }
+  res.send({ code: 0 });
+});
+router.get("/room/list", async (req, res, next) => {
+  let data = req.query;
+  let conf: any = await ModelConfigRoom.find({});
+  res.send(conf);
+});
+router.get("/config/basic", async (req, res, next) => {
+  let data = req.query;
+  let conf: any = await ModelConfig.findOne({});
+  res.send(conf);
+});
+router.post("/config/basic/edit", async (req, res, next) => {
+  let data = req.body;
+  await ModelConfig.updateOne({}, {
+    BP: data.BP,
+    PA: data.PA,
+    PL: data.PL,
+    TO: data.TO,
+    TP: data.TP,
+    ZH: data.ZH,
   });
-  res.send(data2);
+  res.send({ code: 0 });
+});
+router.get("/user/list", async (req, res, next) => {
+  let data = req.query;
+  let list: any = await ModelUser.find({});
+  res.send(list);
+});
+router.get("/animal/list", async (req, res, next) => {
+  let data = req.query;
+  let list: any = await ModelAnimalType.find({});
+  res.send(list);
+});
+router.post("/animal/update", async (req, res, next) => {
+  let data = req.body;
+  await ModelAnimalType.updateOne({ id: data.id }, data);
+  res.send({ code: 0 });
 });
 router.post("/userinfo", async (req, res, next) => {
   let data = req.body;
